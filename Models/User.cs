c8409f671e01579ac810a25acaef6c0b3cfa024b
Jin Mizuno <bump.lk_k.1215@icloud.com>
2024-02-10 18:32:51 +0000

    UserクラスにisAdminカラムを追加しました。

--- Models/User.cs
+++ Models/User.cs
@@ -5,6 +5,7 @@ namespace WADProject1.Models
         public int UserId { get; set; }
         public required string Email { get; set; }
         public required string Password { get; set; }
+        public bool IsAdmin { get; set; }
         public UserProfile? UserProfile { get; set; }
         public ICollection<Swipe> SentSwipes { get; set; }
         public ICollection<Swipe> ReceivedSwipes { get; set; }
