c69a26c5be88403b21761fd4ca4c7f1139b3e6d5
Jin Mizuno <bump.lk_k.1215@icloud.com>
2024-02-10 19:39:03 +0000

    UsersContollerから不必要なAPIエンドポイントを削除しました

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -8,19 +8,6 @@ public class UsersController : ControllerBase
 {
     private readonly TenderContext _context;
 
-    // GET: api/Users
-    public UsersController(TenderContext context)
-    {
-        _context = context;
-    }
-
-    // GET: api/Users
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<User>>> GetUsers()
-    {
-        return await _context.Users.ToListAsync();
-    }
-
     // GET: api/Users/5
     [HttpGet("{id}")]
     public async Task<ActionResult<User>> GetUser(int id)
@@ -33,46 +20,6 @@ public class UsersController : ControllerBase
         }
 
         return user;
-    }
-
-    // PUT: api/Users/5
-    [HttpPut("{id}")]
-    public async Task<IActionResult> PutUser(int id, User user)
-    {
-        if (id != user.UserId)
-        {
-            return BadRequest();
-        }
-
-        _context.Entry(user).State = EntityState.Modified;
-
-        try
-        {
-            await _context.SaveChangesAsync();
-        }
-        catch (DbUpdateConcurrencyException)
-        {
-            if (!UserExists(id))
-            {
-                return NotFound();
-            }
-            else
-            {
-                throw;
-            }
-        }
-
-        return NoContent();
-    }
-
-    // POST: api/Users
-    [HttpPost]
-    public async Task<ActionResult<User>> PostUser(User user)
-    {
-        _context.Users.Add(user);
-        await _context.SaveChangesAsync();
-
-        return CreatedAtAction("GetUser", new { id = user.UserId }, user);
     }
 
     // DELETE: api/Users/5
@@ -89,11 +36,6 @@ public class UsersController : ControllerBase
         await _context.SaveChangesAsync();
 
         return NoContent();
-    }
-
-    private bool UserExists(int id)
-    {
-        return _context.Users.Any(e => e.UserId == id);
     }
 }
 
